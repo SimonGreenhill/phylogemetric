3bc08b139d522bd4cda6ce0a99c86975933f2245
SimonGreenhill <simon@simon.net.nz>
2021-12-01 14:55:47 +1300

    update setup.py

--- setup.py
+++ setup.py
@@ -3,19 +3,14 @@ from setuptools import setup, find_packages
 from codecs import open
 from os import path
 
-here = path.abspath(path.dirname(__file__))
-
 description = "A python library for calculating the delta score (Holland et al. 2002) and Q-Residual (Gray et al. 2010)"
-
-# Get the long description from the relevant file
-with open(path.join(here, 'DESCRIPTION.md'), encoding='utf-8') as f:
-    long_description = f.read()
 
 setup(
     name='phylogemetric',
-    version='1.0.0',
+    version='1.1.0',
     description=description,
-    long_description=long_description,
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     url='https://github.com/SimonGreenhill/phylogemetric',
     author='Simon J. Greenhill',
     author_email='simon@simon.net.nz',
@@ -27,18 +22,27 @@ setup(
         "Topic :: Scientific/Engineering",
         "Topic :: Scientific/Engineering :: Bio-Informatics",
         "Topic :: Software Development :: Libraries :: Python Modules",
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
     keywords='phylogenetics delta-score q-residual',
     packages=find_packages(),
     package_data={'phylogemetric': ['data/*.nex']},
     install_requires=['python-nexus'],
+    extras_require={
+        'dev': ['wheel', 'twine'],
+        'test': [
+            'pytest>=5',
+            'pytest-cov',
+            'coverage>=4.2',
+        ],
+    },
     entry_points={
         'console_scripts': [
             'phylogemetric = phylogemetric.bin.phylogemetric:main'
